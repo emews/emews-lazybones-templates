f49311e0378143115c163f52aa7a8cac94cc6ab7
Nick Collier <ncollier@anl.gov>
2016-08-26 10:39:36 -0400

    Updated to reflect latest EQ/Py changes

--- templates/subtmpl-eqpy/EQPy.swift
+++ templates/subtmpl-eqpy/EQPy.swift
@@ -4,45 +4,45 @@ pragma worktypedef resident_work;
 @dispatch=resident_work
 (void v) _void_py(string code) "turbine" "0.1.0"
     [ "turbine::python 1 <<code>>" ];
-    
+
 @dispatch=resident_work
 (string output) _string_py(string code) "turbine" "0.1.0"
     [ "set <<output>> [ turbine::python 1 <<code>> ]" ];
 
-string init_package_string = "import eqp\nimport %s\n" +
+string init_package_string = "import eqpy\nimport %s\n" +
 "import threading\n" +
 "p = threading.Thread(target=%s.run)\np.start()\n\"\"";
 
 
-(void v) EQP_init_package(location loc, string packageName){
-    // printf("EQP_init_package called");
+(void v) EQPy_init_package(location loc, string packageName){
+    // printf("EQPy_init_package called");
     string code = init_package_string % (packageName,packageName);
     // printf("Code is: \n%s", code);
     @location=loc _void_py(code) => v = propagate();
 }
 
-EQP_stop(location loc){
+EQPy_stop(location loc){
     // do nothing
 }
 
-string get_string = "result = eqp.output_q.get()\nresult";
+string get_string = "result = eqpy.output_q.get()\nresult";
 
-(string result) EQP_get(location loc){
-    // printf("EQP_get called");
+(string result) EQPy_get(location loc){
+    // printf("EQPy_get called");
     string code = get_string;
     result = @location=loc _string_py(code);
 }
 
 string put_string = """
-eqp.input_q.put('%s')\n""
+eqpy.input_q.put('%s')\n""
 """;
 
-(void v) EQP_put(location loc, string data){
-    // printf("EQP_put called with: \n%s", data);
+(void v) EQPy_put(location loc, string data){
+    // printf("EQPy_put called with: \n%s", data);
     string code = put_string % data;
-    // printf("EQP_put code: \n%s", code);
+    // printf("EQPy_put code: \n%s", code);
     @location=loc _void_py(code) => v = propagate();
-}   
+}    
 
 // Local Variables:
 // c-basic-offset: 4
--- templates/subtmpl-eqpy/eqpy.py
+++ templates/subtmpl-eqpy/eqpy.py
@@ -7,49 +7,8 @@ output_q = queue.Queue()
 
 def OUT_put(string_params):
     output_q.put(string_params)
-    
+
 def IN_get():
     global input_q
     result = input_q.get()
     return result    
-
-#     with open("pyout.txt", "a") as myfile:
-#         myfile.write("In in get\n")
-#     global input_q
-#     with open("pyout.txt", "a") as myfile:
-#         myfile.write("after global \n")
-#     try:
-#         result = input_q.get()
-#     except Exception as inst:
-#         with open("pyout.txt", "a") as myfile:
-#             myfile.write(type(inst))
-#        print(type(inst))    # the exception instance
-#        print(inst.args)     # arguments stored in .args
-#        print(inst)
-
-#     with open("pyout.txt", "a") as myfile:
-#         myfile.write("in IN_get\n")
-#     return '2,1,0,"/project/jozik/repos/wintersim2016_adv_tut/uc2/swift_proj/data/params_for_deap.csv"'
-
-
-
-#     global input_q
-#     
-#     with open("pyout.txt", "a") as myfile:
-#         myfile.write("after global 2\n")
-# 
-# 
-#     return "blah"
-#     try:
-#         with open("pyout.txt", "a") as myfile:
-#             myfile.write("in try block\n")
-# 
-#         result = input_q.get()
-#         with open("pyout.txt", "a") as myfile:
-#             myfile.write("result is: {}\n",result)
-#     finally:
-#         with open("pyout.txt", "a") as myfile:
-#             myfile.write("Unexpected error:")
-#     return result
-# 
-
