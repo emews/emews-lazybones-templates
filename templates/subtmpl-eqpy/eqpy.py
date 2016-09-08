1a1de57238fbd8032a00891225ba5bd044c160f1
Nick Collier <ncollier@anl.gov>
2016-09-08 11:19:01 -0400

    Updated to latest eqpy.py

--- templates/subtmpl-eqpy/eqpy.py
+++ templates/subtmpl-eqpy/eqpy.py
@@ -1,14 +1,20 @@
 import threading
-import queue
 import sys
 
-input_q = queue.Queue()
-output_q = queue.Queue()
+try:
+    from queue import Queue
+except ImportError:
+    # queue is Queue in python 2
+    from Queue import Queue
+
+input_q = Queue()
+output_q = Queue()
 
 def OUT_put(string_params):
     output_q.put(string_params)
-    
+
 def IN_get():
     global input_q
     result = input_q.get()
-    return result    
+    return result
+
