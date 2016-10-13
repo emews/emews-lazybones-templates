f97c9b18f606319bd3dcc6c92eecf0a8ce25befd
Nick Collier <ncollier@anl.gov>
2016-10-13 09:43:07 -0400

    Resolved issues #1 and #2 -- improving .sh scripts and logging script and var to experiments directory

--- templates/subtmpl-eqpy/swift_run_eqpy.sh
+++ templates/subtmpl-eqpy/swift_run_eqpy.sh
@@ -8,15 +8,23 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-# uncomment to turn off swift/t logging
-# export TURBINE_LOG=0 TURBINE_DEBUG=0 ADLB_DEBUG=0
+# uncomment to turn on swift/t logging. Can also set TURBINE_LOG,
+# TURBINE_DEBUG, and ADLB_DEBUG to 0 to turn off logging
+# export TURBINE_LOG=1 TURBINE_DEBUG=1 ADLB_DEBUG=1
 export EMEWS_PROJECT_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
+# source some utility functions used by EMEWS in this script
+source "${EMEWS_PROJECT_ROOT}/etc/emews_utils.sh"
+
 export EXPID=$1
 export TURBINE_OUTPUT=$EMEWS_PROJECT_ROOT/experiments/$EXPID
-export PROCS=4   # number of processes
+check_directory_exists
 
-# QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME will
-# be ignored if the -m scheduler flag is not used
+# TODO edit the number of processes as required.
+export PROCS=4
+
+# TODO edit QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME
+# as required. Note that QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME will
+# be ignored if MACHINE flag (see below) is not set
 export QUEUE=batch
 export WALLTIME=00:10:00
 export PPN=16
@@ -24,8 +32,8 @@ export TURBINE_JOBNAME="${EXPID}_job"
 
 # if R cannot be found, then these will need to be
 # uncommented and set correctly.
-#export R_HOME=/soft/R/src/R-3.2.2/
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/soft/R/src/R-3.2.2/lib/
+# export R_HOME=/path/to/R
+# export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$R_HOME/lib
 export PYTHONPATH=$EMEWS_PROJECT_ROOT/python:$EMEWS_PROJECT_ROOT/ext/EQ-Py
 
 # Resident task workers and ranks
@@ -36,8 +44,9 @@ export RESIDENT_WORK_RANKS=$(( PROCS - 2 ))
 EQPY=$EMEWS_PROJECT_ROOT/ext/EQ-Py
 
 # TODO edit command line arguments, e.g. -nv etc., as appropriate
-# for your EQ/Py based run.
-CMD_LINE_ARGS=" -nv=5 -seed=0"
+# for your EQ/Py based run. $* will pass any of this script's command
+# line arguments to swift.
+CMD_LINE_ARGS="$* -nv=5 -seed=0"
 
 # Uncomment this for the BG/Q:
 #export MODE=BGQ QUEUE=default
@@ -50,7 +59,14 @@ if [ -n "$MACHINE" ]; then
   MACHINE="-m $MACHINE"
 fi
 
+# Add any script variables that you want to log as
+# part of the experiment meta data to the USER_VARS array,
+# for example, USER_VARS=("VAR_1" "VAR_2")
+USER_VARS=()
+# log variables and script to to TURBINE_OUTPUT directory
+log_script
+
 # echo's anything following this to standard out
 set -x
 
-swift-t -n $PROCS $MACHINE -p -I $EQPY -r $EQPY \$EMEWS_PROJECT_ROOT/swift/swift_run_eqpy.swift $CMD_LINE_ARGS
+swift-t -n $PROCS $MACHINE -p -I $EQPY -r $EQPY $EMEWS_PROJECT_ROOT/swift/swift_run_eqpy.swift $CMD_LINE_ARGS
--- templates/subtmpl-sweep/swift_run_sweep.sh.gtpl
+++ templates/subtmpl-sweep/swift_run_sweep.sh.gtpl
@@ -9,15 +9,23 @@ if [ "\$#" -ne 1 ]; then
 fi
 
 
-# uncomment to turn off swift/t logging
-# export TURBINE_LOG=0 TURBINE_DEBUG=0 ADLB_DEBUG=0
+# uncomment to turn on swift/t logging. Can also set TURBINE_LOG,
+# TURBINE_DEBUG, and ADLB_DEBUG to 0 to turn off logging
+# export TURBINE_LOG=1 TURBINE_DEBUG=1 ADLB_DEBUG=1
 export EMEWS_PROJECT_ROOT=\$( cd \$( dirname \$0 )/.. ; /bin/pwd )
+# source some utility functions used by EMEWS in this script
+source "\${EMEWS_PROJECT_ROOT}/etc/emews_utils.sh"
+
 export EXPID=\$1
 export TURBINE_OUTPUT=\$EMEWS_PROJECT_ROOT/experiments/\$EXPID
-export PROCS=${proc_count}  # number of processes
+check_directory_exists
 
-# QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME will
-# be ignored if the -m scheduler flag is not used
+# TODO edit the number of processes as required.
+export PROCS=${proc_count}
+
+# TODO edit QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME
+# as required. Note that QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME will
+# be ignored if the MACHINE variable (see below) is not set.
 export QUEUE=batch
 export WALLTIME=00:10:00
 export PPN=16
@@ -25,8 +33,16 @@ export TURBINE_JOBNAME="\${EXPID}_job"
 
 # if R cannot be found, then these will need to be
 # uncommented and set correctly.
-# export R_HOME=/soft/R/src/R-3.2.2/
-# export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/soft/R/src/R-3.2.2/lib/
+# export R_HOME=/path/to/R
+# export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$R_HOME/lib
+# if python packages can't be found, then uncommited and set this
+# export PYTHONPATH=/path/to/python/packages
+
+
+# TODO edit command line arguments, e.g. -nv etc., as appropriate
+# for your EQ/Py based run. Note that \$* will pass any of this
+# script's command line arguments to swift-t
+CMD_LINE_ARGS="\$*"
 
 # set machine to your schedule type (e.g. pbs, slurm, cobalt etc.),
 # or empty for an immediate non-queued unscheduled run
@@ -36,7 +52,14 @@ if [ -n "\$MACHINE" ]; then
   MACHINE="-m \$MACHINE"
 fi
 
+# Add any script variables that you want to log as
+# part of the experiment meta data to the USER_VARS array,
+# for example, USER_VARS=("VAR_1" "VAR_2")
+USER_VARS=()
+# log variables and script to to TURBINE_OUTPUT directory
+log_script
+
 # echo's anything following this standard out
 set -x
 
-swift-t -n \$PROCS \$MACHINE -p \$EMEWS_PROJECT_ROOT/swift/swift_run_sweep.swift -f="${input_file}"
+swift-t -n \$PROCS \$MACHINE -p \$EMEWS_PROJECT_ROOT/swift/swift_run_sweep.swift -f="${input_file}" \$CMD_LINE_ARGS
